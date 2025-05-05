e51a8c5d4559afec600c94ab45459e1ab679cef8
Dipok Kumar Ray <dipok7316@gmail.com>
2025-05-05 16:44:02 +0600

    Hero and Footer section complete

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router"; // এটি ঠিক করো: 'react-router' → 'react-router-dom'
 
 const Footer = () => {
-    return (
-        <div>
-            <h2>This is Footer</h2>
-        </div>
-    );
+  return (
+    <div className="text-center bg-gray-300 py-6">
+      <div className="flex flex-wrap justify-center gap-6 text-lg font-semibold mb-4">
+        <NavLink to="/home" className="link link-hover">
+          Home
+        </NavLink>
+        <NavLink to="/companies" className="link link-hover">
+          Companies
+        </NavLink>
+        <NavLink to="/about" className="link link-hover">
+          About
+        </NavLink>
+        <NavLink to="/privacy" className="link link-hover">
+          Privacy Policy
+        </NavLink>
+      </div>
+      <p className="text-sm">© {new Date().getFullYear()} JobsTrack. All rights reserved.</p>
+    </div>
+  );
 };
 
 export default Footer;
